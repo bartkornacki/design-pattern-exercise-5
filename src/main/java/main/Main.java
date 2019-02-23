9bb3a79e3d912b8b649c2e4f842a8d831866f516
Bartek <bartek.kornacki@gmail.com>
2019-02-23 11:39:37 +0100

    Dodane super komentarze ;)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+//
         System.out.println("== RTV/AGD Store ==");
         Product productOne = new Product("Samsung TV 55",
                 "Samsung TV with a screen size of 55 inches",
